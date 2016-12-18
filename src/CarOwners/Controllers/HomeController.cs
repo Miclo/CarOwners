32413da4efbdbf8eab1cdb3598d607c0e98a37a5
Lorenz Michels <Lorenz.Michels@gmail.com>
2016-12-18 01:36:59 +0100

    added code to controller and model and created view

--- src/CarOwners/Controllers/HomeController.cs
+++ src/CarOwners/Controllers/HomeController.cs
@@ -1,38 +1,71 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarOwners.Models;
+using Microsoft.AspNetCore.Mvc;
 
-/// <summary>
-/// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
-/// </summary>
-/// ///	<remarks>
-///		<remark>
-///			<ID>LM-04</ID>
-///			<date>2016-12-17</date>
-///			<texts>
-///				<text>How to add your first controller</text>
-///				<text>Add Controllers folder to project (right click project and choose Add...</text>
-///				<text>Then right click Controllers folder and choose Add... choose MVC controller</text>
-///			</texts>
-///			<link>
-///				<title>8. (ASP.NET Core 1.0 &amp; MVC) Creating Your First MVC Application</title>
-///				<url>https://www.youtube.com/watch?v=P7LL3OKFmDU&amp;feature=youtu.be&amp;t=42s</url>
-///			</link>
-///		</remark>
-///	</remarks>
+
 namespace CarOwners.Controllers
 {
 	/// <summary>
 	/// Class HomeController
 	/// </summary>
+	/// <summary>
+	/// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+	/// </summary>
+	/// ///	<remarks>
+	///		<remark>
+	///			<ID>LM-04</ID>
+	///			<date>2016-12-17</date>
+	///			<texts>
+	///				<text>How to add your first controller</text>
+	///				<text>Add Controllers folder to project (right click project and choose Add...</text>
+	///				<text>Then right click Controllers folder and choose Add... choose MVC controller</text>
+	///			</texts>
+	///			<link>
+	///				<title>8. (ASP.NET Core 1.0 &amp; MVC) Creating Your First MVC Application</title>
+	///				<url>https://www.youtube.com/watch?v=P7LL3OKFmDU&amp;feature=youtu.be&amp;t=42s</url>
+	///			</link>
+	///		</remark>
+	///		<remark>
+	///			<ID>LM-06</ID>
+	///			<date>2017-12-17</date>
+	///			<texts>
+	///				<text>Instantiate model Make and add data --> dataobject</text>
+	///			</texts>
+	///			<link>
+	///				<title>8. (ASP.NET Core 1.0 &amp; MVC) Creating Your First MVC Application</title>
+	///				<url>https://www.youtube.com/watch?v=P7LL3OKFmDU&amp;feature=youtu.be&amp;t=4m9s</url>
+	///			</link>
+	///			<code>Make make =...</code>
+	///		</remark>
+	///		<remark>
+	///			<ID>LM-07</ID>
+	///			<date>2017-12-17</date>
+	///			<texts>
+	///				<text>Add the dataobject as a parameter to the view</text>
+	///			</texts>
+	///			<link>
+	///				<title>8. (ASP.NET Core 1.0 &amp; MVC) Creating Your First MVC Application</title>
+	///				<url>https://www.youtube.com/watch?v=P7LL3OKFmDU&amp;feature=youtu.be&amp;t=5m18s</url>
+	///			</link>
+	///			<code>return View(make)</code>
+	///		</remark>
+	///	</remarks>
 	public class HomeController : Controller
-    {
-		
+	{
+
 		/// <summary>
-		/// GET controller
+		/// Get controller
 		/// </summary>
 		/// <returns></returns>
 		public IActionResult Index()
-        {
-            return View();
-        }
-    }
+		{
+
+			Make make = new Make()
+			{
+				ID = 1,
+				Name = "Ford"
+			};
+			return View(make);
+		}
+	}
 }
+
